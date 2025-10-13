58d29621539c9d8c60b0678128dc9321a915c0b7
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-10-13 19:50:01 +0800

    feat: 侧边栏地址更新 & 新增赞助页面

--- docs/.vuepress/components/AsideNav.vue
+++ docs/.vuepress/components/AsideNav.vue
@@ -1,37 +1,33 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { VPLink } from 'vuepress-theme-plume/client'
-import { useRouteLocale } from 'vuepress/client'
 
-interface Locale {
-  star: string
-  issue: string
-  sponsor: string
-}
-
-const locales: Record<string, Locale> = {
-  '/': { star: '在 GitHub 上 Star', issue: '遇到问题？', sponsor: '喝杯奶茶' },
-}
-
-const lang = useRouteLocale()
-const locale = computed(() => locales[lang.value])
 </script>
 
 <template>
   <div class="aside-nav-wrapper">
+    <VPLink class="link" no-icon href="https://user.mslmc.net">
+      <i class="fa-solid fa-user"></i>
+      <span class="link-text">MSLFrp/MSL用户中心</span>
+      <span class="vpi-arrow-right" />
+    </VPLink>
+    <VPLink class="link" href="/article/qq/">
+      <i class="fa-brands fa-qq"></i>
+      <span class="link-text">加入交流群</span>
+      <span class="vpi-arrow-right" />
+    </VPLink>
     <VPLink class="link" no-icon href="https://github.com/MSLTeam/MSL">
-      <span class="vpi-github-star" />
-      <span class="link-text">{{ locale.star }}</span>
+      <i class="fa-regular fa-star"></i>
+      <span class="link-text">在 GitHub 上 Star</span>
       <span class="vpi-arrow-right" />
     </VPLink>
     <VPLink class="link" no-icon href="https://github.com/MSLTeam/MSL/issues/new/choose">
-      <span class="vpi-github-issue" />
-      <span class="link-text">{{ locale.issue }}</span>
+      <i class="fa-solid fa-circle-question"></i>
+      <span class="link-text">遇到问题 ?</span>
       <span class="vpi-arrow-right" />
     </VPLink>
     <VPLink class="link" href="/sponsor/">
-      <span class="vpi-bubble-tea" />
-      <span class="link-text">{{ locale.sponsor }}</span>
+      <i class="fa-solid fa-mug-saucer"></i>
+      <span class="link-text">赞助一下 !</span>
       <span class="vpi-arrow-right" />
     </VPLink>
   </div>
@@ -62,17 +58,5 @@ const locale = computed(() => locales[lang.value])
 .aside-nav-wrapper .link .link-text {
   flex: 1 2;
   font-size: 12px;
-}
-
-.vpi-github-star {
-  --icon: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m12 1.5l3.1 6.3l6.9 1l-5 4.8l1.2 6.9l-6.2-3.2l-6.2 3.2L7 13.6L2 8.8l6.9-1z'/%3E%3C/svg%3E");
-}
-
-.vpi-github-issue {
-  --icon: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'%3E%3Cpath fill='%23000' d='M8 9.5a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3'/%3E%3Cpath fill='%23000' d='M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M1.5 8a6.5 6.5 0 1 0 13 0a6.5 6.5 0 0 0-13 0'/%3E%3C/svg%3E");
-}
-
-.vpi-bubble-tea {
-  --icon: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m17.95 9l-1.478 8.69c-.25 1.463-.374 2.195-.936 2.631c-1.2.931-6.039.88-7.172 0c-.562-.436-.687-1.168-.936-2.632L5.95 9M6 9l.514-1.286a5.908 5.908 0 0 1 10.972 0L18 9M5 9h14m-7 0l4-7m-5.99 12h.01m1 4h.01m1.99-2h.01'/%3E%3C/svg%3E");
 }
 </style>
---
+++ docs/blog/MSL/sponsor.md
@@ -0,0 +1,21 @@
+---
+title: 赞助我们的开发
+createTime: 2025/10/13 16:05:46
+permalink: /sponsor/
+tags:
+  - 软件资讯
+aside: false
+---
+
+::: important 赞助我们的开发
+
+==MSL=={.important}是一款简易的MC开服器，旨在帮助各位服主轻松开启一个属于您自己的MC服务器。
+
+如果觉得MSL还不错，可以给个[✨Star](https://github.com/MSLTeam/MSL)呀~
+
+自2021年首个版本发布以来，我们不断为此更新，如果您有能力，可以小小的赞助一下我们的开发~
+
+:::
+
+<LinkCard icon="mug-hot" title="前往爱发电赞助" href="https://afdian.com/a/mslteam" />
+
